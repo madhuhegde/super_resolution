58e79b54cb21e3117fe0ad04b72d92f125f32fa9
Madhusudan Hegde <madhu.hegde@gmail.com>
2023-12-22 14:17:48 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -248,7 +248,7 @@ install_requires = [
 version_range_max = max(sys.version_info[1], 10) + 1
 
 setup(
-    name="latent_diffusion",
+    name="latentdiffusion",
     version="0.1.0.dev0",  # expected format is one of x.y.z.dev0, or x.y.z.rc1 or x.y.z (no to dashes, yes to dots)
     description="State-of-the-art diffusion in PyTorch and JAX.",
     long_description=open("README.md", "r", encoding="utf-8").read(),
